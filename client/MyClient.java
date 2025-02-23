4f8ecef0d120a6537656d7731b8638a1e97aa94c
Eric Kim <kimeric657@gmail.com>
2025-02-22 16:34:24 -1000

    finished

--- client/MyClient.java
+++ client/MyClient.java
@@ -1,25 +1,50 @@
+/**
+ * NAME        : Eric Kim
+ *  
+ * HOMEWORK    : 3
+ * 
+ * CLASS       : ICS 451
+ * 
+ * INSTRUCTOR  : Ravi Narayan
+ * 
+ * DATE        : 02/22/2025
+ * 
+ * FILE        : MyClient.java
+ * 
+ * DESCRIPTION : This file contains the client-side code for homework 3.
+ *
+ */
 import java.io.*;
 import java.net.*;
 
+    /**
+     * The main method to start the client.
+     *
+     * @param args Command line arguments. Expects the port number of the server.
+     */
 public class MyClient {
     public static void main(String[] args) {
+        // Check if the user provided the correct number of arguments
         if (args.length != 1) {
             System.out.println("Usage: java MyClient <port>");
             return;
         }
 
+        // Parse the port number from the command line arguments
         int port = Integer.parseInt(args[0]);
         File receivedFile = new File("client_file.txt");
 
+        // Try to connect to the server and receive the file
         try (Socket socket = new Socket("localhost", port);
              InputStream inputStream = socket.getInputStream();
              FileOutputStream fileOutput = new FileOutputStream(receivedFile)) {
 
             System.out.println("Connected to server. Receiving file...");
 
-            
+            // Buffer to hold file data
             byte[] buffer = new byte[4096];
             int bytesRead;
+            // Read file data and save it to the file
             while ((bytesRead = inputStream.read(buffer)) != -1) {
                 fileOutput.write(buffer, 0, bytesRead);
             }
--- server/MyServer.java
+++ server/MyServer.java
@@ -1,29 +1,57 @@
+/**
+ * NAME        : Eric Kim
+ *  
+ * HOMEWORK    : 3
+ * 
+ * CLASS       : ICS 451
+ * 
+ * INSTRUCTOR  : Ravi Narayan
+ * 
+ * DATE        : 02/22/2025
+ * 
+ * FILE        : MyServer.java
+ * 
+ * DESCRIPTION : This file contains the server-side code for homework 3.
+ *
+ */
+
 import java.io.*;
 import java.net.*;
 
 public class MyServer {
+
+    /**
+     * The main method to start the server.
+     *
+     * @param args Command line arguments. Expects the port number for which the server will be on.
+     */
     public static void main(String[] args) {
+        // Check if the user provided the correct number of arguments
         if (args.length != 1) {
             System.out.println("Usage: java MyServer <port>");
             return;
         }
-
+        
+        // Parse the port number from the command line arguments
         int port = Integer.parseInt(args[0]);
         File fileToSend = new File("server_file.txt");
 
+        // Try to create a server socket and listen for incoming connections
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server started. Waiting for connections on port " + port + "...");
 
-            while (true) {  
+            // Accept incoming connections and send the file
+            while (true) { 
                 try (Socket clientSocket = serverSocket.accept();
                      FileInputStream fileInput = new FileInputStream(fileToSend);
                      OutputStream outputStream = clientSocket.getOutputStream()) {
 
                     System.out.println("Client connected: " + clientSocket.getInetAddress());
 
-                    
+                    // Buffer to hold file data
                     byte[] buffer = new byte[4096];
                     int bytesRead;
+                    // Read file data and send it to the client
                     while ((bytesRead = fileInput.read(buffer)) != -1) {
                         outputStream.write(buffer, 0, bytesRead);
                     }
